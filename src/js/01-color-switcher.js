53828821d21e4b779d38cbe4fc062faee9c617c2
bober2005 <127338926+bober2005@users.noreply.github.com>
2023-08-25 10:51:19 +0300

    js-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const bodyEl = document.querySelector("body");
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector("button[data-stop]");
+let timerId = null;
+let firstRun = true;
 
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', runColor);
+buttonStop.addEventListener('click', stopColor);
+
+function runColor(){
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    timerId = setInterval(colorChange, 1000);
+    if (firstRun){
+        colorChange();
+        firstRun = false;
+    };
+};
+
+function stopColor(){
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+    firstRun = true;
+};
+
+
+function colorChange(){
+    bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
